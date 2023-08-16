60831de17eee626916d2cfeff8063bb7f8c7412b
Medet <medet.berikbaev.94@gmail.com>
2023-08-16 18:00:43 +0600

    move the homework

--- tasks/medet/destructure.js
+++ tasks/medet/destructure.js
@@ -1,1 +1,43 @@
-///
+
+//===============Подсчет количество своиств=======================================
+let car = {
+    name: 'Toyota',
+    year:  2005,
+    color: 'white',
+    engine: '2.4L'
+
+};
+function count() {
+    return Object.keys(car).length
+};
+console.log( count() );
+
+//=========================Деструктурирующее присваивание===========================
+
+let user = {
+    name: 'John',
+    year: 30,
+};
+
+let {name: name, year: age, isAdmin = false} = user;
+
+//==========================Максимальная зарплата===================================
+
+let salaries = {
+    'John': 300,
+    'Pete': 250,
+    'Mark': 280,
+    'Andrew': 310
+};
+function getMaxSalary() {
+    let maxSalary = 0;
+    let maxName = null;
+    for(let [name, salary] of Object.entries(salaries) ) {
+        if(maxSalary < salary) {
+            maxSalary = salary;
+            maxName = name;
+        }
+    }
+    return maxName;
+}
+console.log( getMaxSalary() )
