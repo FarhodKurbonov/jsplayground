a63d4c0b38e23917b31eb5209591591ebb94a786
Medet <medet.berikbaev.94@gmail.com>
2023-08-24 13:57:46 +0600

    Recursion

--- src/pages/lessons/5.11/tasks/medet/index.js
+++ src/pages/lessons/5.11/tasks/medet/index.js
@@ -4,8 +4,8 @@ console.log(date);
 
 // 1) task ==== Create date================
 
-let date = new Date(2012, 1, 20, 3, 12, );
-console.log(date);
+let date1 = new Date(2012, 1, 20, 3, 12, );
+console.log(date1);
 
 // 2) task =========Покажите день недели======
 
@@ -49,7 +49,7 @@ function lastDayOfMonth(year, month) {
 
 // 6) task ================Сколько сегодня прошло секунд? ==============
 
-let today = new Date() - new Date(2023, 7, 23, 00, 00, 00);
+let today = new Date() - new Date(2023, 7, 23, 0, 0, 0);
 console.log(today / 1000); // это первый вариант без функции, чтоб разобраться
 
 
@@ -66,16 +66,17 @@ function secondsOfDay() {
 
 // 7) task ========Сколько секунд осталось до завтра?=========================
 
-function secondsOfDay() {
+function secondsOfDay2() {
     let now = new Date();
   
-    let tmorrow = new Date(2023, 7, 24, 00, 00, 00);
+    let tmorrow = new Date(2023, 7, 24, 0, 0, 0);
   
     let minus = now - tmorrow;
   
     return minus / 1000;
   };
-  console.log( secondsOfDay() ); // решение почти такое же как в предыдущем задании
+  console.log( secondsOfDay2() ); // решение почти такое же как в предыдущем задании
+
 
   // 8) task ===============Форматирование относительной даты=================
 
@@ -101,3 +102,27 @@ return str.replace();
 
 console.log( formatDate(new Date(new Date - 4000))); 
 
+// -========= Recursion============-
+//Вычислить сумму чисел до данного==
+
+function sumTo(n) {
+  let result = 0;
+  for(let i = 1; i < n; i++) {
+    result = i
+  };
+  return result + n
+};
+console.log( sumTo(9) );
+
+// Fibonacci===-
+
+function fibonacci(n) {
+  let result = [0, 1];
+  for(let i = 2; i <= n; i++) {
+    const num1 = result[i - 1]
+    const num2 = result[i - 2]
+    result.push(num1 + num2)
+  }
+  return result[n];
+};
+console.log( fibonacci(15) )
