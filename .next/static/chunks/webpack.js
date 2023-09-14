643a14f26c84a4d90a2d77956fdccfccc642bee4
Sidduqbek Hasanov <hasanoff507@gmail.com>
2023-09-14 13:01:26 +0500

    some changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f05ab9563dab52b7"; }
+/******/ 		__webpack_require__.h = function() { return "e306affaa1427fd9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
+++ .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "0196cfdd64734873"; }
+/******/ 	__webpack_require__.h = function() { return "e306affaa1427fd9"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
